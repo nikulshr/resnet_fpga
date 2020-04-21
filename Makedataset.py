1c60197c8c4d45853239e417eeb6b4a605a20497
ZSenZhang <625382031@sjtu.edu.cn>
2020-04-21 15:45:34 -0700

    figure out the bug

--- Makedataset.py
+++ Makedataset.py
@@ -1,3 +1,5 @@
+from torch.utils import data
+import h5py
 # X is the 2D input and Y is the classification result
 # This func just append them together into a dataset.
 def make_dataset(file_path, train=True):
@@ -6,26 +8,24 @@ def make_dataset(file_path, train=True):
   dataset = []
 
   if train:
-    with h5py.File(file_path) as h5_file:
-      for gname, group in h5_file.items():
-          # print(group)
-        datasets.append(group)
-  
-      idx = 0
-      for things in datasets[0]:
-        dataset.append(datasets[0][idx],datasets[1][idx])
-        idx = idx +1
+    fhd5 = h5py.File(file_path,'r')
+    for gname, group in fhd5.items():
+      datasets.append(group)
+    idx = 0
+    for things in datasets[0]:
+      # dataset.append(datasets[0][idx],datasets[1][idx])
+      dataset.append([idx,idx])
+      idx = idx +1
   
   else:
-    with h5py.File(file_path) as h5_file:
-      for gname, group in h5_file.items():
-          # print(group)
-        datasets.append(group)
-  
-      idx = 0
-      for things in datasets[0]:
-        dataset.append(datasets[0][idx],datasets[1][idx])
-        idx = idx +1
+    fhd5 = h5py.File(file_path,'r')
+    for gname, group in fhd5.items():
+      datasets.append(group)
+    idx = 0
+    for things in datasets[0]:
+      dataset.append([idx,idx])
+      # dataset.append(datasets[0][idx],datasets[1][idx])
+      idx = idx +1
 # # 
   return dataset
 
@@ -34,18 +34,32 @@ class MyDataSet(data.Dataset):
   def __init__(self, file_path, transform = None, train = True):
     self.train = train
     if self.train:
+      traindatasets = []
+      fhd5 = h5py.File(file_path,'r')
+      for gname, group in fhd5.items():
+          # print(group)
+        traindatasets.append(group)
       self.train_set_path = make_dataset(file_path, train)
     else:
+      testdatasets = []
+      fhd5 = h5py.File(file_path,'r')
+      for gname, group in fhd5.items():
+          # print(group)
+        testdatasets.append(group)
       self.test_set_path = make_dataset(file_path, train)
 
   def __getitem__(self, index):
     if self.train:
-      img, label = self.train_set_path[index]
+      img_path, label_path = self.train_set_path[index]
+      img = self.traindatasets[0][img_path]
+      label = self.traindatasets[0][label_path]
       if self.transform is not None:
         img = self.transform(img)
       return img,label
     else:
-      img, label = self.test_set_path[index]
+      img_path, label_path = self.test_set_path[index]
+      img = self.testdatasets[0][img_path]
+      label = self.testdatasets[0][label_path]
       if self.transform is not None:
         img = self.transform(img)
       return img,label
