ae54cc3967a9a334cc995e9a0daf979ebfe72bea
ZSenZhang <625382031@sjtu.edu.cn>
2020-04-21 12:41:36 -0700

    parsing data and make dataset code

---
+++ Makedataset.py
@@ -0,0 +1,58 @@
+def make_dataset(file_path, train=True):
+ 
+  datasets = []
+  dataset = []
+
+  if train:
+    with h5py.File(file_path) as h5_file:
+      for gname, group in h5_file.items():
+          # print(group)
+        datasets.append(group)
+  
+      idx = 0
+      for things in datasets[0]:
+        dataset.append(datasets[0][idx],datasets[1][idx])
+        idx = idx +1
+  
+  else:
+    with h5py.File(file_path) as h5_file:
+      for gname, group in h5_file.items():
+          # print(group)
+        datasets.append(group)
+  
+      idx = 0
+      for things in datasets[0]:
+        dataset.append(datasets[0][idx],datasets[1][idx])
+        idx = idx +1
+# # 
+  return dataset
+
+class MyDataSet(data.Dataset):
+  def __init__(self, file_path, transform = None, train = True):
+    self.train = train
+    if self.train:
+      self.train_set_path = make_dataset(file_path, train)
+    else:
+      self.test_set_path = make_dataset(file_path, train)
+
+  def __getitem__(self, index):
+    if self.train:
+      img, label = self.train_set_path[index]
+      if self.transform is not None:
+        img = self.transform(img)
+      return img,label
+    else:
+      img, label = self.test_set_path[index]
+      if self.transform is not None:
+        img = self.transform(img)
+      return img,label
+
+  def __len__(self):
+    if self.train:
+      return len(self.train_set_path) 
+
+    else:
+      return len(self.test_set_path)
+
+
+
---
+++ parsedata.py
@@ -0,0 +1,58 @@
+import h5py
+
+dataset =[]
+with h5py.File('GOLD_XYZ_OSC.0001_1024.hdf5') as h5_file:
+	for gname, group in h5_file.items():
+		print(group)
+		dataset.append(group)
+	# print(dataset[0][1])
+
+	ftrain = h5py.File('traintest.h5','w')
+	ftest = h5py.File('testset.h5','w')
+	testXset = []
+	trainXset = []
+	trainYset = []
+	testYset = []
+	Ynum = 0
+	index = 0
+	count = 0
+	flag = True
+	# print(dataset[1][index][Ynum])
+
+	for data in dataset[1]:
+		if dataset[1][index][Ynum] == 0:
+			for tmpY in range(0,23):
+				if dataset[1][index][tmpY] == 1:
+					Ynum = tmpY
+					flag = True
+					print("find next one")
+					break
+		if dataset[1][index][Ynum] == 1 and flag == True:
+			testXset.append(dataset[0][index])
+			testYset.append(dataset[1][index])
+			# Ynum = Ynum+1
+			index = index + 1
+			count = count + 1
+			if count == 1000:
+				flag = False
+				count = 0
+				print(str(Ynum) + ":testsetdone")
+				# Ynum = Ynum + 1
+
+		else:
+			trainXset.append(dataset[0][index])
+			trainYset.append(dataset[1][index])
+			index = index + 1
+		
+		if index % 1000 == 0:
+			print(index/2555904)
+
+
+	# print(dataset)
+
+	ftrain.create_dataset('X',data = trainXset)
+	ftrain.create_dataset('Y',data = trainYset)
+	ftest.create_dataset('X',data = testXset)
+	ftest.create_dataset('Y',data = testYset)
+
+
