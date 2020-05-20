686e67e3b51db83e680751f9e1a2f5f9d1e1d9be
ZSenZhang <625382031@sjtu.edu.cn>
2020-05-19 19:13:19 -0700

    dense

---
+++ dataforfpga/dataforfpga/dense0.h
@@ -0,0 +1,3 @@
+#define DENSE0_IN 128
+#define DENSE0_OUT 1024
+short * dense0( const short in[DENSE0_IN], short out[DENSE0_OUT] );
---
+++ dataforfpga/dataforfpga/dense2.h
@@ -0,0 +1,3 @@
+#define DENSE2_IN 24
+#define DENSE2_OUT 128
+short * dense2( const short in[DENSE2_IN], short out[DENSE2_OUT] );
